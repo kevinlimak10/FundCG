351157afd45f504332994ad09a27795e78696417
Kevin Lima <kevinlima53@gmail.com>
2022-11-21 18:10:08 -0300

    iniciando o projeto

---
+++ Trabalho_GB/filter.py
@@ -0,0 +1,61 @@
+import cv2 as cv
+import numpy as np
+from numpy import array
+
+def brilho(img, beta_value ):
+    img_bright = cv.convertScaleAbs(img, beta=beta_value)
+
+    return img_bright
+
+def grayscale(image):
+    converted_image = np.array(image)
+    gray_image = cv.cvtColor(converted_image, cv.COLOR_RGB2GRAY)
+    return gray_image
+
+def sketch(image):
+    converted_image = np.array(image)
+    gray_image = cv.cvtColor(converted_image, cv.COLOR_RGB2GRAY)
+    inv_gray_image = 255 - gray_image
+    blur_image = cv.GaussianBlur(inv_gray_image, (21, 21), 0, 0)
+    sketch_image = cv.divide(gray_image, 255 - blur_image, scale=256)
+    return sketch_image
+
+
+def sepia(image):
+    converted_image = np.array(image)
+    converted_image = cv.cvtColor(converted_image, cv.COLOR_RGB2BGR)
+    kernel = np.array([[0.272, 0.534, 0.132],
+                       [0.349, 0.686, 0.168],
+                       [0.393, 0.769, 0.189]])
+    sepia_image = cv.filter2D(converted_image, -1, kernel)
+    sepia_image = cv.cvtColor(sepia_image, cv.COLOR_BGR2RGB)
+    return sepia_image
+
+
+def blur(image):
+    b_amount = 9
+    converted_image = np.array(image)
+    converted_image = cv.cvtColor(converted_image, cv.COLOR_RGB2BGR)
+    blur_image = cv.GaussianBlur(converted_image, (b_amount, b_amount), 0, 0)
+    blur_image = cv.cvtColor(blur_image, cv.COLOR_BGR2RGB)
+    return blur_image
+
+
+def canny(image):
+    threshold1 = 100
+    threshold2 = 150
+    converted_image = np.array(image)
+    converted_image = cv.cvtColor(converted_image, cv.COLOR_RGB2BGR)
+    blur_image = cv.GaussianBlur(converted_image, (11, 11), 0)
+    canny_image = cv.Canny(blur_image, threshold1, threshold2)
+    return canny_image
+
+
+def original(image):
+    return image
+
+
+def tranformTo3d(img):
+    data = array(img)
+    data = data.reshape((data, data, 1))
+    return data
--- Trabalho_GB/main.py
+++ Trabalho_GB/main.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+import numpy as np
+from filter import grayscale, original, sketch, sepia, blur, canny
 
 video = cv.VideoCapture(0)
 
@@ -9,25 +11,49 @@ writer= cv.VideoWriter('stories.mp4', cv.VideoWriter_fourcc(*'mp4v'), 20, (width
 
 record = False
 
+filters = {
+    '0': original,
+    '1': grayscale,
+    '2': sketch,
+    '3': sepia,
+    '4': blur,
+    '5': canny,
+    '6': None,
+    '7': None
+}
 
+selected_filter = '0'
 while True:
     check,frame = video.read()
-    gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+
+    frameToPreFilter = cv.resize(frame,(240,160))
+
+    filter = filters.get(selected_filter)
+    if filter is not None:
+        frame = filter(frame)
+    previews = [original(frameToPreFilter),grayscale(frameToPreFilter),sketch(frameToPreFilter),sepia(frameToPreFilter),blur(frameToPreFilter),canny(frameToPreFilter)]
+
+    img_h1 = cv.hconcat([previews[0],previews[3],previews[4]])
+    img_h2 = cv.hconcat([previews[1],previews[2],previews[5]])
+    # img_final = cv.hconcat([img_h1,img_h2])
+
     if record:
         writer.write(frame)
         cv.putText(frame, 'Press Space to stop Record', (0, 130), 4, 1, (200, 255, 155))
     else:
         cv.putText(frame, 'Press R to Start Record or Q to exit', (0, 130), 4, 1, (200, 255, 155))
-    cv.imshow("smile meter", frame)
+    cv.imshow("Stories", img_h1)
 
     key = cv.waitKey(1)
-    if (key == ord('r') and not record):
+    if key == ord('r') and not record:
         record = True
-    if (key == ord(' ') and record):
+    if key == ord(' ') and record:
         record = False
         writer.release()
-    if(key == ord('q')):
+    if key == ord('q'):
         break
+    if key in [ord(k) for k in filters.keys()]:
+        selected_filter = chr(key)
 
 video.release()
 cv.destroyAllWindows
