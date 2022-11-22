04f116420785f82b3204867cf4a22a1f46ab3a4a
Kevin lima <kevinlima@asav-72-99.unisinos.br>
2022-11-21 21:48:24 -0300

    adicionando codigo funcionando

--- Trabalho_GB/main.py
+++ Trabalho_GB/main.py
@@ -4,12 +4,25 @@ from filter import grayscale, original, sketch, sepia, blur, canny
 
 video = cv.VideoCapture(0)
 
-width= int(video.get(cv.CAP_PROP_FRAME_WIDTH))
-height= int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
+width = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
+sizeHeader = int(height * 0.2)
 
-writer= cv.VideoWriter('stories.mp4', cv.VideoWriter_fourcc(*'mp4v'), 20, (width,height))
+writer = cv.VideoWriter('stories.mp4', cv.VideoWriter_fourcc(*'mp4v'), 20, (width, height))
 
 record = False
+
+posList = []
+
+
+def onMouse(event, x, y, flags, param):
+    global posList
+    if event == cv.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+
+def checkChangeFilter(x,y):
+    return
+
 
 filters = {
     '0': original,
@@ -17,38 +30,52 @@ filters = {
     '2': sketch,
     '3': sepia,
     '4': blur,
-    '5': canny,
-    '6': None,
-    '7': None
+    '5': canny
 }
 
+widthHeader = int(width / len(filters))
 selected_filter = '0'
 while True:
-    check,frame = video.read()
-
-    frameToPreFilter = cv.resize(frame,(240,160))
+    check, frame = video.read()
+    frameToPreFilter = cv.resize(frame, (widthHeader, sizeHeader))
 
     filter = filters.get(selected_filter)
     if filter is not None:
         frame = filter(frame)
-    previews = [original(frameToPreFilter),grayscale(frameToPreFilter),sketch(frameToPreFilter),sepia(frameToPreFilter),blur(frameToPreFilter),canny(frameToPreFilter)]
 
-    img_h1 = cv.hconcat([previews[0],previews[3],previews[4]])
-    img_h2 = cv.hconcat([previews[1],previews[2],previews[5]])
-    img_preview = cv.hconcat([img_h1,img_h2])
+    previews = [original(frameToPreFilter),
+                grayscale(frameToPreFilter),
+                sketch(frameToPreFilter),
+                sepia(frameToPreFilter),
+                blur(frameToPreFilter),
+                canny(frameToPreFilter)]
 
-    #get final size
-    frame = cv.resize(frame,((len(previews) * 240), 680))
+    img_preview = cv.hconcat([previews[0], previews[1], previews[2], previews[3], previews[4], previews[5]])
 
-    img_final = cv.vconcat([img_preview,frame])
-    cv.resize(frame, (240, 160))
+    # resize to original size
+    img_preview = cv.resize(img_preview, (width, sizeHeader))
+    # get final size
+    frame = cv.resize(frame, (width, height - sizeHeader))
+
+    img_final = cv.vconcat([img_preview, frame])
+
+
+    # cv.resize(frame, (240, 160))
+
+    if len(posList) > 0:
+        x,y = posList[0]
+        if y > 0 and x > 0 and y <= sizeHeader and x <= width:
+            selected_filter = str(int(x/widthHeader))
+        posList = []
 
     if record:
         writer.write(frame)
         cv.putText(frame, 'Press Space to stop Record', (0, 130), 4, 1, (200, 255, 155))
     else:
         cv.putText(frame, 'Press R to Start Record or Q to exit', (0, 130), 4, 1, (200, 255, 155))
-    cv.imshow("Stories", img_final)
+    cv.imshow("stories", img_final)
+    cv.setMouseCallback('stories', onMouse)
+
 
     key = cv.waitKey(1)
     if key == ord('r') and not record:
@@ -58,9 +85,11 @@ while True:
         writer.release()
     if key == ord('q'):
         break
+
     if key in [ord(k) for k in filters.keys()]:
         selected_filter = chr(key)
 
+
+
 video.release()
 cv.destroyAllWindows
-
