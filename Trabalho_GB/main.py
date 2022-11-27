107e130c549ad9740645d42904db76b17d247373
Thiago Monaco <thiago.monaco@cwi.com.br>
2022-11-27 12:19:53 -0300

    Removendo texto

--- Trabalho_GB/main.py
+++ Trabalho_GB/main.py
@@ -1,9 +1,6 @@
 import cv2 as cv
 from sticker import *
 
-cv.useOptimized()
-cv.setUseOptimized(True)
-
 video = cv.VideoCapture(0)
 width= int(video.get(cv.CAP_PROP_FRAME_WIDTH))
 height= int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
@@ -11,8 +8,6 @@ height= int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
 initialImg = cv.imread('stickers/mopaz.png')
 cv.imshow("smile meter", initialImg)
 cv.createTrackbar('Sticker', "smile meter", 0, len(stickersList), handleStickerIndex)
-mbox_bottom = lambda text: cv.displayStatusBar("smile meter",text,  1500)
-mbox_bottom("Press 'q' to quit")
 
 while True:
     check,frame = video.read()
